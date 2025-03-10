e9240f6f061cb01d8a7f6f7b815488ad099863d4
ArnauVilella <avp@connect.ust.hk>
2025-03-10 18:27:32 +0800

    Update version to 0.6.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="trexselector",
-    version="0.6.1",
+    version="0.6.4",
     author="Python port by Arnau Vilella",
     author_email="avp@connect.ust.hk",
     description="T-Rex Selector: High-Dimensional Variable Selection & FDR Control",
